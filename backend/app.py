b532299a436a0671504b17aa7e91f886739c9bbc
lucaas-d3v <lucas.workps@gmail.com>
2026-05-19 19:34:18 -0300

    feat: adicionado cadastro de usuario

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,85 @@
 from flask import Flask, request, jsonify
 import pymysql
 
+import bcrypt as cpt
+
 app = Flask(__name__)
 
-def connect():
-    db = pymysql.connect(host='localhost', user='root', password='12345678', db='rpg')
-    return db
+# Banco
+class Conn:
+    def __init__(self):
+        self.connection = ""
+        self.cursor = ""
 
+    def init(self):
+        self.connection = pymysql.connect(host='localhost', user='root', password='12345678', database='rpg')
+        self.cursor = self.connection.cursor()
 
+        return self
+
+    def deinit(self):
+        self.cursor.close()
+        self.connection.close()
+
+# API
 @app.route('/')
 def home():
-    return {
-        "mensagem": "API de RPG funcionando!"
-    }
+    return jsonify (
+        {
+            "status": "online",
+            "mensagem": "API de RPG funcionando!"
+        }
+    )
+
+@app.route("/registrar", methods=["POST"])
+def registrar():
+    """
+        A Rota espera o seguinte json:
+
+        {
+            "nome": "",
+            "email": "",
+            "senha": ""
+        }    
+    """
+
+    data = request.get_json()
+
+    nome = data["nome"]
+    email = data["email"]
+    senha_digitada = data["senha"]
+
+    # verificações de segurança
+    if (nome is None or email is None or senha_digitada is None) or (len(nome) == 0 or len(email) == 0 or len(senha_digitada) == 0 ):
+        return jsonify(
+            {
+                "mensagem": "O nome, email e senha são obrigatórios!"
+            }
+        ), 400
+
+    # gera hash seguro da senha
+    bytes_senha = senha_digitada.encode('utf-8')
+    salt = cpt.gensalt()
+    senha_hash = cpt.hashpw(bytes_senha, salt).decode('utf-8')
+
+    # conecta com o banco e fecha conexões de forma segura
+    conn = Conn().init()
+
+    sql = "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)"
+    conn.cursor.execute(sql, (nome, email, senha_hash))
+    conn.connection.commit()
+
+    conn.deinit()
+
+    return jsonify(
+        {
+            "mensagem": "Usuário criado com sucesso"  
+        }
+    ), 201
+
+# RO-dar
 if __name__ == '__main__':
+    con = Conn().init()
+    con.deinit()
+
     app.run(debug=True)
