094657dd8b97a72d36e31eda2c8a714fa1339bb8
lucaas-d3v <lucas.workps@gmail.com>
2026-05-20 20:50:57 -0300

    feat: sistema de recuperação de senha

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,29 @@
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_mail import Mail,  Message
 from dotenv import load_dotenv
 
 import pymysql
 import os
 import bcrypt as cpt
+import random as r
 
 load_dotenv()
 
 app = Flask(__name__)
+
+# cofigs
 app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'paulinolucas668@gmail.com'
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWD")
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
 jwt = JWTManager(app)
+mail = Mail(app)
 
 # Banco
 class Conn:
@@ -65,9 +78,7 @@ def registrar():
         ), 400
 
     # gera hash seguro da senha
-    bytes_senha = senha_digitada.encode('utf-8')
-    salt = cpt.gensalt()
-    senha_hash = cpt.hashpw(bytes_senha, salt).decode('utf-8')
+    senha_hash = hashear(senha_digitada)
 
     # conecta com o banco e fecha conexões de forma segura
     conn = Conn().init()
@@ -140,9 +151,130 @@ def entrar():
         }
     ), 201
 
+@app.route("/recuperar-senha", methods=["POST"])
+def rec_senha():
+    """
+        A Rota espera o seguinte json:
+
+        {
+            "email": ""
+        }
+    """
+
+    dados = request.get_json()
+    email = dados["email"]
+
+    code_gen = __rec_envia_codigo(email)
+
+    if code_gen == 1:
+        raise Exception("Err")
+
+    return jsonify(
+        {
+            "mensagem": "Codigo enviada com sucesso",
+            "codigo": code_gen
+        }
+    )
+
+
+@app.route("/utils/verifica-codigo", methods=["POST"])
+def rec_verifica_codigo():
+    """
+        A Rota espera o seguinte json:
+
+        {
+            "codigo_digitado": "",
+            "codigo-real": ""
+        }
+    """
+
+    dados = request.get_json()
+
+    try:
+        code = dados["codigo_digitado"]
+        if code is None or len(code) == 0:
+            return jsonify(
+                {
+                    "mensagem": "O Código é obrigatório"
+                }
+            ), 400
+        
+        code_d = int(code)
+        code_r = int(dados["codigo_real"])
+        
+    except Exception as e:
+        raise Exception(f"Erro: {e}")
+
+    r = code_d == code_r
+
+    if not r:
+        return jsonify(
+            {
+                "mensagem": "Código inválido"
+            }
+        ), 401
+    
+    return jsonify(
+        {
+            "mensagem": "Código confimado"
+        }
+    ), 200
+
+@app.route("/utils/atualizar-senha", methods=["PATCH"])
+def atualizar_senha():
+    dados = request.json
+
+    # assume que o front mandou o email tbm
+    if "senha" not in dados:
+        return jsonify(
+            {
+                "mensagem": "A nova senha é obrigatória!"
+            }
+        ), 400
+
+    sql = "UPDATE usuarios SET senha = %s WHERE email = %s"
+    conn = Conn().init()
+    conn.cursor.execute(sql, (hashear(dados["senha"]), dados["email"]))
+    conn.connection.commit()
+
+    conn.deinit()
+
+    return jsonify(
+        {
+            "mensagem": "Nova senha atualizada com sucesso!"
+        }
+    ), 200
+
+
+# utils
+def __rec_envia_codigo(email: str):
+    msg = Message(
+        subject='Código de Confirmação',
+        sender='paulinolucas668@gmail.com',
+         recipients=[email]
+    )
+
+    code = r.randint(1000, 9999)
+    msg.body = f"O seu código de confirmação é: {code}"
+
+    try: 
+        mail.send(msg)
+        return code
+    except Exception as e:
+        print(e)
+        return 1
+
+def hashear(senha_digitada):
+    bytes_senha = senha_digitada.encode('utf-8')
+    salt = cpt.gensalt()
+    return cpt.hashpw(bytes_senha, salt).decode('utf-8')
+
+
 # Rodar
 if __name__ == '__main__':
     con = Conn().init()
     con.deinit()
+    
+    print("igual" if "$12$ar1IOFHGj3YMszpn41GMpuEIYivcTBOuKH8yfuSW6UiMGUIGh.M1m" == "$2b$12$MT2JWt5TTipK318tMbklI.2V/lqfet8a7tHcQoywshXIMmTiqS97m" else "diferente")
 
     app.run(debug=True)
